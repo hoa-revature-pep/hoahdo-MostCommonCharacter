9b7ced200b5c5abd9a3e34e70a841e31cdf93a82
Hoa Do <hoa.do.ma@gmail.com>
2023-09-05 11:45:54 +0000

    Completed Challenge: Most Common Character

--- src/main/java/MostCommonCharacter.java
+++ src/main/java/MostCommonCharacter.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class MostCommonCharacter {
     /**
@@ -8,6 +10,31 @@ public class MostCommonCharacter {
      * @return the most common character within str.
      */
     public char recurringChar(String str) {
-        return ' ';
+
+        char mostCommonChar = ' '; 
+        int commonCharCount = 0;
+        char[] strArr = str.toCharArray();
+        Map<Character, Integer> hMap = new HashMap<Character, Integer>();
+        
+        for (char character : strArr) {
+            if (hMap.containsKey(character)) {
+                hMap.put(character, hMap.get(character) + 1);
+
+            } else {
+                hMap.put(character, 1);
+            }
+        }
+        
+        for (Map.Entry<Character, Integer> currentEntry : hMap.entrySet()) {
+            char char_Key = currentEntry.getKey();
+            int count_Value = currentEntry.getValue();
+            
+            if (count_Value > commonCharCount) {
+                commonCharCount = count_Value;
+                mostCommonChar = char_Key;
+            }
+        }
+
+        return mostCommonChar;
     }
 }
